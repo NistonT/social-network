304623e3dafe9ec59ae2d694c78c138594ffe360
NistonN <nisprogrammer@gmail.com>
2026-05-06 20:38:40 +0300

    [0002] fix

--- src/widgets/Registration/index.tsx
+++ src/widgets/Registration/index.tsx
@@ -1,19 +1,17 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-
 export const Registration = () => {
-  const onSubmitRegister: SubmitHandler<TAuth> = async (data) => {
-    await createUserWithEmailAndPassword(auth, data.email, data.password);
-  };
+  // const onSubmitRegister: SubmitHandler<TAuth> = async (data) => {
+  //   await createUserWithEmailAndPassword(auth, data.email, data.password);
+  // };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmitRegister)}>
+      {/* <form onSubmit={handleSubmit(onSubmitRegister)}>
         <input type="email" className="border-black border-2" {...register("email", { required: "Введите почту" })} />
         <input type="password" className="border-black border-2" {...register("password", { required: "Введите пароль" })} />
         <button type="submit" className="border-black border-2">
           Register
         </button>
-      </form>
+      </form> */}
     </div>
   );
 };
