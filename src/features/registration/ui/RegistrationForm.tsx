e187955800c8199d206c22daa231fbb7757a6699
NistonN <nisprogrammer@gmail.com>
2026-05-15 21:43:28 +0300

    [0005] a registration form has been created

--- src/features/registration/ui/RegistrationForm.tsx
+++ src/features/registration/ui/RegistrationForm.tsx
@@ -1,22 +1,49 @@
-import { ButtonForm, InputForm } from "@/shared/ui";
+import { ButtonForm, ErrorField, InputForm } from "@/shared/ui";
 import { KeyRound, Mail, User } from "lucide-react";
+import { useRegistration } from "../hooks/useRegistration";
 
 export const RegistrationForm = () => {
+  const { register, handleSubmit, errors, isSubmitted, onSubmitRegister } = useRegistration();
+
   return (
     <div className="w-full">
-      <form className="flex flex-col gap-3 w-full">
-        <div>
-          <InputForm icon={User} type="text" placeholder="Display name" />
+      <form onSubmit={handleSubmit(onSubmitRegister)} className="flex flex-col gap-3 w-full">
+        <div className="flex flex-col">
+          <InputForm
+            icon={User}
+            type="text"
+            placeholder="Display name"
+            {...register("displayName")}
+            aria-invalid={!!errors.displayName}
+            aria-describedby={errors.displayName ? "displayName-error" : undefined}
+          />
+          <ErrorField field={errors.displayName} id={"displayName-error"} />
         </div>
         <div className="flex flex-col">
-          <InputForm icon={Mail} type="email" placeholder="email@burmalda.com" />
+          <InputForm
+            icon={Mail}
+            type="email"
+            placeholder="email@burmalda.com"
+            {...register("email")}
+            aria-invalid={!!errors.email}
+            aria-describedby={errors.email ? "email-error" : undefined}
+          />
+          <ErrorField field={errors.email} id={"email-error"} />
         </div>
         <div>
-          <InputForm icon={KeyRound} type="password" placeholder="Password" />
+          <InputForm
+            icon={KeyRound}
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+            aria-invalid={!!errors.password}
+            aria-describedby={errors.password ? "password-error" : undefined}
+          />
+          <ErrorField field={errors.password} id={"password-error"} />
         </div>
         <div className="flex items-center mt-5 h-full gap-1">
-          <ButtonForm type="submit" className="flex-1 max-h-11">
-            Create
+          <ButtonForm type="submit" className="flex-1 max-h-11" disabled={isSubmitted}>
+            {isSubmitted ? "Created..." : "Create"}
           </ButtonForm>
         </div>
       </form>
